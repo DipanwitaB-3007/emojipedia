9fddf5ff56277dbfae1e95329178db8eb41c2d5b
Dipanwita Barik <61846454+DipanwitaB-3007@users.noreply.github.com>
2020-05-30 01:02:38 +0530

    Add files via upload

---
+++ src/components/Entry.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Entry(props) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label={props.name}>
+          {props.img}
+        </span>
+        <span>{props.name}</span>
+      </dt>
+      <dd>{props.desc}</dd>
+    </div>
+  );
+}
+
+export default Entry;
---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing."
+  },
+  {
+    id: 4,
+    emoji: "🍒",
+    name: "Cherries",
+    meaning:
+      "She is always cherry-picking. You are the cherry on the cake! The summer fruit can also stand for love because of the heart shape. People often see it as a symbol of full lips and the kiss. Cherries are also known as the forbidden fruit. In Japan, they are a symbol for self-discovery."
+  },
+  {
+    id: 5,
+    emoji: "💚",
+    name: "Green Heart",
+    meaning:
+      "TExpression of life or lifestyle (e.g. healthy diet) and closeness to nature. Also known as the jealous heart, it may indicate relationship issues or the hope for reconciliation. Often used on the Irish St. Patrick's Day."
+  },
+  {
+    id: 6,
+    emoji: "🐧",
+    name: "Penguin",
+    meaning:
+      "The penguin is a symbol of coldness. Tells a person that he or she is particularly awkward, gawkish or clumsy. Also symbolizes balance and reason. Can also announce an addition to the family."
+  }
+];
+
+export default emojipedia;
